4fedf9b77858cb936077af267eb0e0f8084ee8cd
Joshua Akan-Etuk <joshuaakanetuk@gmail.com>
2020-10-16 19:24:38 -0400

    everything but 9

--- queue.js
+++ queue.js
@@ -9,6 +9,35 @@ class Queue {
     this.first = null;
     this.last = null;
   }
+  peek() {
+    if (this.first === null) {
+      this.first = node;
+    }
+    return this.first;
+  }
+  isEmpty() {
+    if (this.first === null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  display() {
+    let print = "";
+    if (this.isEmpty() || this.first === null) {
+      return null;
+    }
+    let currNode = this.first;
+    if (currNode.next === null) {
+      return currNode.value;
+    }
+    while (currNode !== null) {
+      print = print + currNode.value + " -> ";
+      currNode = currNode.next;
+    }
+
+    return print;
+  }
   enqueue(data) {
     const node = new _Node(data);
 
@@ -34,3 +63,105 @@ class Queue {
     return node.value;
   }
 }
+
+let starTrekQ = new Queue();
+starTrekQ.enqueue("Kirk");
+starTrekQ.enqueue("Spock");
+starTrekQ.enqueue("Uhura");
+starTrekQ.enqueue("Sulu");
+starTrekQ.enqueue("Checkov");
+
+console.log(starTrekQ.display());
+
+starTrekQ.dequeue();
+starTrekQ.dequeue();
+
+console.log(starTrekQ.display());
+
+class _DoubleNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+  enqueue(data) {}
+  dequeue() {}
+}
+
+function percent() {
+    return Math.floor(Math.random() * 4) + 1  
+}
+
+class Bank {
+    constructor() {
+      this.first = null;
+      this.last = null;
+    }
+    peek() {
+      if (this.first === null) {
+        this.first = node;
+      }
+      return this.first;
+    }
+    isEmpty() {
+      if (this.first === null) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    display() {
+      let print = "";
+      if (this.isEmpty() || this.first === null) {
+        return null;
+      }
+      let currNode = this.first;
+      if (currNode.next === null) {
+        return currNode.value;
+      }
+      while (currNode !== null) {
+        print = print + currNode.value + " -> ";
+        currNode = currNode.next;
+      }
+  
+      return print;
+    }
+    enqueue(data) {
+      const node = new _Node(data);
+  
+      if (this.first === null) {
+        this.first = node;
+      }
+  
+      if (this.last) {
+        this.last.next = node;
+      }
+      this.last = node;
+    }
+    dequeue() {
+      if (this.first === null) {
+        return;
+      }
+
+      if(percent() === 1){
+        this.enqueue(node.value)
+        return;
+      }
+
+      const node = this.first;
+      this.first = this.first.next;
+      //if this is the last item in the queue
+      if (node === this.last) {
+        this.last = null;
+      }
+
+      
+      return node.value;
+    }
+  }
+
+let starTrekQ = new Bank();
+starTrekQ.enqueue("Kirk");
+starTrekQ.enqueue("Spock");
+starTrekQ.enqueue("Uhura");
+starTrekQ.enqueue("Sulu");
--- stack.js
+++ stack.js
@@ -143,3 +143,33 @@ numbers.push(1);
 numbers.push(2);
 
 console.log(sortStack(numbers));
+
+class Queue {
+    constructor() {
+      this.stack1 = new Stack()
+      this.stack2 = new Stack()
+    }
+  
+    enqueue(item) {
+      this.stack1.push(item)
+    } 
+  
+    dequeue() {
+      if(!this.stack2.top) {
+        if(!this.stack1.top) return 'There is nothing to dequeue!'
+        while(this.stack1.top) {
+          let p = this.stack1.pop()
+          this.stack2.push(p)
+        }
+      }
+      return this.stack2.pop()
+    }
+  }
+  
+  const queue = new Queue()
+  
+  queue.enqueue(2)
+  queue.enqueue(3)
+  queue.enqueue(4)
+  
+  console.log(queue.dequeue())
